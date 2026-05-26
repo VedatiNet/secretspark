c94e276557777903ab265132dcf85718168d6cbd
VedatiNet <130935232+VedatiNet@users.noreply.github.com>
2026-05-26 13:31:26 +0200

    Delete vercel_install.sh

--- vercel_install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-if [ ! -d "flutter" ]; then
-  git clone https://github.com/flutter/flutter.git -b stable --depth 1 flutter
-fi
-./flutter/bin/flutter config --enable-web
-./flutter/bin/flutter pub get
